975dd9bf38ebd20afc0a95d5f9528d90ef93fc98
Alexander <alshmex209@gmail.com>
2023-07-04 23:15:10 -0500

    feat: add smooth scrolling to navbar

--- portfolio/src/app/Components/navbar/navbar.component.ts
+++ portfolio/src/app/Components/navbar/navbar.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+toAbout(){
+  document.getElementById("about")?.scrollIntoView({behavior:"smooth"});
+}
+toHome(){
+  document.getElementById("home")?.scrollIntoView({behavior:"smooth"});
+}
+toProjects(){
+  document.getElementById("projects")?.scrollIntoView({behavior:"smooth"});
+}
+toContact(){
+  document.getElementById("contact")?.scrollIntoView({behavior:"smooth"});
+}
 
 }
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
@@ -23,7 +23,8 @@ import { ProjectsComponent } from './Components/projects/projects.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
